3449cb86966f7484af93cd2c9e38a113ba409119
Timo Lindhorst <timo.lindhorst@travelping.com>
2019-06-05 23:34:24 +0200

    wip

--- infrastructure/openstack/cluster.tf
+++ infrastructure/openstack/cluster.tf
@@ -27,10 +27,6 @@ resource "openstack_compute_instance_v2" "master" {
   }
 
   network {
-    name = "tenant-bgp"
-  }
-
-  network {
     name = "uplink"
   }
 
@@ -38,40 +34,39 @@ resource "openstack_compute_instance_v2" "master" {
     name = "${openstack_networking_network_v2.cluster-network.name}"
   }
 
-  connection {
-      user = "ubuntu"
-      type = "ssh"
-      timeout = "2m"
-  }
-
-	provisioner "file" {
-		source = "libexec/iface-config.sh"
-		destination = "/tmp/iface-config.sh"
-	}
-	provisioner "remote-exec" {
-		inline = [
-			"sudo sed -i 's/^root:.:/root:ADAHBG3GsnVKU:/' /etc/shadow",
-			"sudo chmod 0700 /tmp/iface-config.sh",
-			"sudo /tmp/iface-config.sh --udev shared0 ${self.network.0.mac}",
-			"sudo /tmp/iface-config.sh --udev bgp0 ${self.network.1.mac}",
-			"sudo /tmp/iface-config.sh --udev uplink0 ${self.network.2.mac}",
-			"sudo /tmp/iface-config.sh --udev cluster0 ${self.network.3.mac}",
-			"sudo /tmp/iface-config.sh --ipconfig shared0 dhcp",
-			"sudo /tmp/iface-config.sh --ipconfig bgp0 dhcp",
-			"sudo /tmp/iface-config.sh --ipconfig uplink0 dhcp",
-			"sudo /tmp/iface-config.sh --ipconfig cluster0 dhcp",
-			"sudo reboot"
-		]
-	}
-
-	provisioner "remote-exec" {
-		inline = [
-			"export PATH=$PATH:/usr/bin",
-			"sudo apt-get -y update",
-			"sudo apt-get -y install python2.7",
-			"sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1",
-		]
-	}
+#  connection {
+#    user = "ubuntu"
+#    type = "ssh"
+#    timeout = "2m"
+#    host = "${self.access_ip_v4}"
+#  }
+#
+#	provisioner "file" {
+#		source = "libexec/iface-config.sh"
+#		destination = "/tmp/iface-config.sh"
+#	}
+#	provisioner "remote-exec" {
+#		inline = [
+#			"sudo sed -i 's/^root:.:/root:ADAHBG3GsnVKU:/' /etc/shadow",
+#			"sudo chmod 0700 /tmp/iface-config.sh",
+#			"sudo /tmp/iface-config.sh --udev shared0 ${self.network.0.mac}",
+#			"sudo /tmp/iface-config.sh --udev uplink0 ${self.network.1.mac}",
+#			"sudo /tmp/iface-config.sh --udev cluster0 ${self.network.2.mac}",
+#			"sudo /tmp/iface-config.sh --ipconfig shared0 dhcp",
+#			"sudo /tmp/iface-config.sh --ipconfig uplink0 dhcp",
+#			"sudo /tmp/iface-config.sh --ipconfig cluster0 dhcp",
+#			"sudo reboot"
+#		]
+#	}
+#
+#	provisioner "remote-exec" {
+#		inline = [
+#			"export PATH=$PATH:/usr/bin",
+#			"sudo apt-get -y update",
+#			"sudo apt-get -y install python2.7",
+#			"sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1",
+#		]
+#	}
 }
 
 # worker nodes
@@ -92,10 +87,6 @@ resource "openstack_compute_instance_v2" "worker" {
   }
 
   network {
-    name = "tenant-bgp"
-  }
-
-  network {
     name = "uplink"
   }
 
@@ -104,9 +95,10 @@ resource "openstack_compute_instance_v2" "worker" {
   }
 
   connection {
-      user = "ubuntu"
-      type = "ssh"
-      timeout = "2m"
+    user = "ubuntu"
+    type = "ssh"
+    timeout = "2m"
+    host = "${self.access_ip_v4}"
   }
 
 	provisioner "file" {
@@ -118,25 +110,39 @@ resource "openstack_compute_instance_v2" "worker" {
 			"sudo sed -i 's/^root:.:/root:ADAHBG3GsnVKU:/' /etc/shadow",
 			"sudo chmod 0700 /tmp/iface-config.sh",
 			"sudo /tmp/iface-config.sh --udev shared0 ${self.network.0.mac}",
-			"sudo /tmp/iface-config.sh --udev bgp0 ${self.network.1.mac}",
-			"sudo /tmp/iface-config.sh --udev uplink0 ${self.network.2.mac}",
-			"sudo /tmp/iface-config.sh --udev cluster0 ${self.network.3.mac}",
+			"sudo /tmp/iface-config.sh --udev uplink0 ${self.network.1.mac}",
+			"sudo /tmp/iface-config.sh --udev cluster0 ${self.network.2.mac}",
+			"sudo /tmp/iface-config.sh --udev dp0 ${openstack_networking_port_v2.port[count.index].mac_address}",
 			"sudo /tmp/iface-config.sh --ipconfig shared0 dhcp",
-			"sudo /tmp/iface-config.sh --ipconfig bgp0 dhcp",
 			"sudo /tmp/iface-config.sh --ipconfig uplink0 dhcp",
 			"sudo /tmp/iface-config.sh --ipconfig cluster0 dhcp",
-			"sudo reboot"
+			"sudo shutdown -r +0"
 		]
 	}
+#
+#	provisioner "remote-exec" {
+#		inline = [
+#			"export PATH=$PATH:/usr/bin",
+#			"sudo apt-get -y update",
+#			"sudo apt-get -y install python2.7",
+#			"sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1",
+#		]
+#	}
+}
 
-	provisioner "remote-exec" {
-		inline = [
-			"export PATH=$PATH:/usr/bin",
-			"sudo apt-get -y update",
-			"sudo apt-get -y install python2.7",
-			"sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1",
-		]
-	}
+resource "openstack_networking_port_v2" "port" {
+  count           = "${var.workercount}"
+  name            = "${format("%s-worker-port-%02d", var.name_prefix, count.index+1)}"
+  network_id      = "${openstack_networking_network_v2.underlay-network.id}"
+  no_security_groups = "true"
+  no_fixed_ip = "true"
+  port_security_enabled = "false"
+}
+
+resource "openstack_compute_interface_attach_v2" "attach" {
+  count           = "${var.workercount}"
+  instance_id = "${openstack_compute_instance_v2.worker.*.id[count.index]}"
+  port_id  = "${openstack_networking_port_v2.port.*.id[count.index]}"
 }
 
 resource "openstack_networking_network_v2" "cluster-network" {
@@ -144,14 +150,32 @@ resource "openstack_networking_network_v2" "cluster-network" {
   admin_state_up = "true"
 }
 
+resource "openstack_networking_network_v2" "underlay-network" {
+  name = "${var.name_prefix}-underlay-network"
+  admin_state_up = "true"
+  #port_security_enabled = "false"
+}
+
 variable "cluster_cidr" {
   default = "10.77.0.0/18"
+}
+
+variable "underlay_cidr" {
+  default = "10.78.0.0/18"
 }
 
 resource "openstack_networking_subnet_v2" "cluster-network" {
   name = "${var.name_prefix}-cluster-network"
   network_id = "${openstack_networking_network_v2.cluster-network.id}"
   cidr = "${var.cluster_cidr}"
+  ip_version = 4
+  no_gateway = "true"
+}
+
+resource "openstack_networking_subnet_v2" "underlay-network" {
+  name = "${var.name_prefix}-underlay-network"
+  network_id = "${openstack_networking_network_v2.underlay-network.id}"
+  cidr = "${var.underlay_cidr}"
   ip_version = 4
   no_gateway = "true"
 }
--- infrastructure/openstack/provider.tf
+++ infrastructure/openstack/provider.tf
@@ -1,3 +1,4 @@
 provider "openstack" {
   tenant_name = "tp-development"
+  version = "~> 1.19"
 }
